fa36f0f32b38557a2f0bd8a8c47555a5e350e285
Timothy Chin <timothychin328@gmail.com>
2020-09-01 22:21:25 -0500

    Committing first C++ programs

---
+++ hellotests.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char** argv)
+{
+    cout << "Hello Test!" << endl;
+    return 0;
+
+    
+}
---
+++ ifstatements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x = 201;
+    int y = 21;
+    if (x > y) {
+        cout << "x is greater than y \n";
+    } else {
+        cout << "\n NOT";
+    }
+}
